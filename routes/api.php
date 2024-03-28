8d6056ea8c35ac0ff98334f7410b4655b6638edc
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-28 16:12:46 +0100

    fix import controller file api.php , add function index for call api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Api\ProjectController;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('projects', ProjectController::class);
+Route::apiResource('projects', ProjectController::class);
